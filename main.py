7e1573c2be35cc8c0288e0a3afc642a0819fe9ad
g-bulgarit <alonio6@gmail.com>
2020-04-20 00:46:35 +0300

    changed "pixel size" to hardcoded 20, fixed typos

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def get_sprite_dict():
         gray_image = image_file.convert("L")
         image_matrix = np.asarray(gray_image)
         avg_brightness = image_matrix.sum()//image_matrix.shape[0]**2
-        sprite_dict[avg_brightness] = gray_image
+        sprite_dict[avg_brightness] = gray_image.resize((20, 20))
         image_file.close()
 
     return sprite_dict
@@ -21,17 +21,15 @@ def get_sprite_dict():
 def gray_resized(in_image, percent):
     pic_file = Image.open(in_image)
     gray_image = pic_file.convert("L")
-    width, hight = gray_image.size
+    width, height = gray_image.size
     percent_left = 100 - percent
-    new_width = (width * percent_left // 100) // 50 * 50
-    new_hight = (hight * percent_left // 100) // 50 * 50
-    new_size = (new_width, new_hight)
+    new_width = (width * percent_left // 100) // 20 * 20
+    new_height = (height * percent_left // 100) // 20 * 20
+    new_size = (new_width, new_height)
     gray_image_resized = gray_image.resize(new_size)
     return gray_image_resized
 
 
-def matrix_values(input_img, sprites):
-    gray_image_resized = gray_resized(input_img, 90)
 def matrix_values(input_img, sprites, resize_value):
     gray_image_resized = gray_resized(input_img, resize_value)
     sprite_dict_values = list(sprites.keys())
@@ -44,21 +42,20 @@ def matrix_values(input_img, sprites, resize_value):
 
 
 def create_frame(image_matrix, sprites):
-    img_hight = image_matrix.shape[0]
+    img_height = image_matrix.shape[0]
     img_width = image_matrix.shape[1]
-    img_size = (img_width * 50, img_hight * 50)
+    img_size = (img_width * 20, img_height * 20)
     output_img = Image.new('L', img_size, color=0)
-    for row in range(img_hight):
+    for row in range(img_height):
         for col in range(img_width):
             value = image_matrix[row][col]
-            output_img.paste(sprites[value], box=(col * 50, row * 50))
+            output_img.paste(sprites[value], box=(col * 20, row * 20))
     return output_img
 
 
 def main():
     img_path = "Koala.jpg"
     sprite_dictionary = get_sprite_dict()
-    color_matrix = matrix_values(img_path, sprite_dictionary)
     color_matrix = matrix_values(img_path, sprite_dictionary, 85)
 
     constructed_frame = create_frame(color_matrix, sprite_dictionary)
